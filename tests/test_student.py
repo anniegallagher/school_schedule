b8e1a93457f9cf5bf30e3a146863651ca421357b
Annie <annie.gallagher7@gmail.com>
2022-10-04 11:44:12 -0600

    added 3 unit tests

--- tests/test_student.py
+++ tests/test_student.py
@@ -9,3 +9,36 @@ def test_adding_one_class_increases_class_length():
     ellis.add_class("Pottery")
 
     assert ellis.get_num_classes() == 2
+    assert ellis.classes == ["Painting", "Pottery"]
+    assert "Pottery" in ellis.classes
+
+# write 3 more unit tests
+
+def test_init_sets_student_data():
+    name = "Ellis"
+    grade = "junior"
+    classes = ["Painting", "Geometry"]
+
+    ellis = Student(name, grade, classes)
+
+    assert ellis.name == name
+    assert ellis.grade == grade
+    assert ellis.classes == classes
+
+def test_get_num_classes_returns_correct_length():
+    name = "Ellis"
+    grade = "junior"
+    classes = ["Painting", "Geometry", "Basket Weaving", "Table Tennis"]
+
+    ellis = Student(name, grade, classes)
+
+    assert ellis.get_num_classes() == 4
+
+def test_display_classes_returns_classes_as_strings():
+    name = "Ellis"
+    grade = "junior"
+    classes = ["Basket Weaving", "Table Tennis"]
+
+    ellis = Student(name, grade, classes)
+
+    assert ellis.display_classes() == "Basket Weaving, Table Tennis"
