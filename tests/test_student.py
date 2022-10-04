9bef916663f1eb3ed0d28ccca6f48852833fa364
Annie <annie.gallagher7@gmail.com>
2022-10-04 11:55:40 -0600

    added test for comparing students

--- tests/test_student.py
+++ tests/test_student.py
@@ -1,4 +1,5 @@
-from school_schedule.student import Student
+from school_schedule.student import *
+
 
 def test_adding_one_class_increases_class_length():
     name = "Ellis"
@@ -42,3 +43,21 @@ def test_display_classes_returns_classes_as_strings():
     ellis = Student(name, grade, classes)
 
     assert ellis.display_classes() == "Basket Weaving, Table Tennis"
+
+# - create a function that will return the student with more classes
+# - create a test for that function
+
+def test_return_student_with_more_classes():
+    name = "Ellis"
+    grade = "junior"
+    classes = ["Basket Weaving", "Table Tennis"]
+    name2 = "Julia"
+    grade2 = "freshman"
+    classes2 = ["Basket Weaving", "Table Tennis", "Drama"]
+
+    ellis = Student(name, grade, classes)
+    julia = Student(name2, grade2, classes2)
+
+    student = student_with_more_classes(ellis, julia)
+
+    assert student == julia
